811c3317a26d2254264465d23a09111a097abce5
Lyuda Kim <lyudarkim@gmail.com>
2023-06-26 14:08:28 -0700

    Implement to_dict() function in Board model

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,10 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "board_id": self.board_id,
+            "title": self.title,
+            "owner": self.owner
+        }
