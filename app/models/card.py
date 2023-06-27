e2deac0f6f3ab104f2a79453f39f372a5ff7451c
selene castillo <secast@live.com>
2023-06-27 10:45:08 -0700

    implement to_dict
    Co-authored-by: sophiat8832 <sophiat8832@users.noreply.github.com>
    Co-authored-by: Lyuda Kim <lyudarkim@users.noreply.github.com>

--- app/models/card.py
+++ app/models/card.py
@@ -4,3 +4,10 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    
+    def to_dict(self):
+        return {
+            "card_id": self.card_id,
+            "message": self.message,
+            "likes_count": self.likes_count
+        }
