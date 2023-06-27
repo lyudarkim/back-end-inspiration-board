6dde1cb16147468f1ef21a417fdf2481abc4f25d
Sophia Tran <sophiat8832@gmail.com>
2023-06-27 11:43:46 -0700

    Establish one-to-many relationship between board and card; add new migration

--- app/models/card.py
+++ app/models/card.py
@@ -4,6 +4,8 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer, default=0)
+    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=True)
+    board = db.relationship("Board", back_populates="cards")
     
     def to_dict(self):
         return {
