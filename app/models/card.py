b2f6747a669669fa05deef80c5e8a3395449fc07
selene castillo <secast@live.com>
2023-06-27 10:59:46 -0700

    Implement from_dict

--- app/models/card.py
+++ app/models/card.py
@@ -3,7 +3,7 @@ from app import db
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     
     def to_dict(self):
         return {
@@ -11,3 +11,9 @@ class Card(db.Model):
             "message": self.message,
             "likes_count": self.likes_count
         }
+    
+    @classmethod
+    def from_dict(cls, card_details):
+        new_card = cls(
+            message=card_details["message"])
+        return new_card
