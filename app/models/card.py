4513621dfba74548d59145135b32ac4f529c74be
Sophia Tran <sophiat8832@gmail.com>
2023-06-28 10:41:42 -0700

    Remove likes count from Card model

--- app/models/card.py
+++ app/models/card.py
@@ -3,7 +3,6 @@ from app import db
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=False)
     board = db.relationship("Board", back_populates="cards")
     
